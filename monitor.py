2bfed01c60c26447a9a1a86ee39357316f4f93b5
Steve Seguin <steve@seguin.email>
2019-01-22 19:25:24 -0500

    Update monitor.py
    fix a bug; also removed specific user functionality -- just sends all messages now.

--- monitor.py
+++ monitor.py
@@ -14,33 +14,26 @@ import datetime
 slack_token =  "xoxp-xxxxxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 sc = SlackClient(slack_token)
 
-def get_messages_for_recipient(id):
-	
+def get_messages():
 	db_path = expanduser("~") + '/Library/Messages/chat.db'
 	connection = sqlite3.connect(db_path)
 	c = connection.cursor()
-	
-	#c.execute("SELECT * FROM `handle`")
-	#for row in c:
-	#	print(row)   ### FIND THE USER ID FOR THE USER YOU WANT
-    	
-	cc = c.execute("SELECT * FROM `message` INNER JOIN handle h ON h.ROWID=message.handle_id WHERE handle_id=" + str(id) + " ORDER BY date DESC LIMIT 5")
+
+	cc = c.execute("SELECT * FROM `message` INNER JOIN handle h ON h.ROWID=message.handle_id ORDER BY date DESC LIMIT 10")
 	messages = []
 	
 	for row in c:
 		messages.append([row])
-			
-	#print(messages)
+
 	connection.close()
 	return reversed(messages)
-
 
 def loop_check():
 	lastmsg = ""
 	print("Starting")
 	while True:
 		old=True
-		messages = imessage.get_messages_for_recipient(int(10))
+		messages = get_messages()
 		for msg in messages:
 			msg=msg[0]
 			if lastmsg==msg[0]:
@@ -75,7 +68,3 @@ def loop_check():
 			pass
 
 loop_check()
-
-
-
-
